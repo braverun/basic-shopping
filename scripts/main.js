e18f4a57506c7d239059837a7fd5d32ce5fa55f5
Weston Humphreys <braverun@gmail.com>
2015-04-22 21:11:52 -0400

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+(function(){
+
+  var Beer = function (name, brewery, cost) {
+    this.name = name;
+    this.brewery = brewery;
+    this.cost = cost;
+  };
+
+  var sculpin = new Beer('Sculpin', 'Balast Point Brewing', 4);
+  var outlaw = new Beer('Oulaw', 'Payette Brewing', 3);
+
+
+}());
