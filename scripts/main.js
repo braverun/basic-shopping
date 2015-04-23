b8a197a1b61ad8266111b22a6194c09305113f7e
Weston Humphreys <braverun@gmail.com>
2015-04-23 13:32:33 -0400

    Print data to page

--- scripts/main.js
+++ scripts/main.js
@@ -1,13 +1,22 @@
 (function(){
 
-  var Beer = function (name, brewery, cost) {
-    this.name = name;
-    this.brewery = brewery;
-    this.cost = cost;
-  };
+var Beer = function (name, brewery, cost) {
+  this.name = name;
+  this.brewery = brewery;
+  this.cost = cost;
+};
 
-  var sculpin = new Beer('Sculpin', 'Balast Point Brewing', 4);
-  var outlaw = new Beer('Oulaw', 'Payette Brewing', 3);
+var sculpin = new Beer('Sculpin', 'Balast Point Brewing', 4);
+var outlaw = new Beer('Oulaw', 'Payette Brewing', 3);
 
+var beers = [sculpin, outlaw];
 
+console.log(beers);
+
+var menuTemplate = _.template($('#menu-template').text());
+var $menu = $('#js-menu');
+
+_.each(beers, function(beer){
+  $menu.append(menuTemplate(beer));
+});
 }());
